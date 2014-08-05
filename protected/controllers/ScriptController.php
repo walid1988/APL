53ea60daf83a45490ebca158c492871413b9262c
mastouri <mohamedaymen.mastouri@esprit.tn>
2014-08-05 16:00:01 +0100

    mas_05081559

--- protected/controllers/ScriptController.php
+++ protected/controllers/ScriptController.php
@@ -1,50 +1,48 @@
 <?php
 
+class ScriptController extends Controller {
 
-class ScriptController extends Controller
-{
- 
+    public function accessRules() {
+        return array(
+            array('allow', // allow all users to perform 'index' and 'view' actions
+                'actions' => array('outbound'),
+                'users' => array('*'),
+            ),
+        );
+    }
 
- public function accessRules()
- {
-  return array(
-   array('allow',  // allow all users to perform 'index' and 'view' actions
-    'actions'=>array('outbound'),
-    'users'=>array('*'),
-   ),
-   
-  );
- }
+    public function actionOutbound() {
+        $presses = Press::model()->findAll();
+        foreach ($presses as $press) {
+            $date = date("Y-m-d H:i:s");
+            if (($press->press_status == 'Q') && ($press->press_date_started <= $date)) {
+                $list = $press->list;
+                $contacts = $list->contacts;
+                $content = '';
+                foreach ($contacts as $contact) {
+                    $email = Yii::app()->mandrillwrap;
+                    $content = $press->press_content;
+                    $content= htmlentities($content);
+                    echo $content;
+                    echo '******';
+                    //                  $content = str_replace('"', '', $htmontent);
+                  $content = substr($content,0,30);
+                  echo $content;
+                  $email->html = $content;//"Hallo, okkkkkkkkkkkkkkkkk ";
+                    $email->subject = $press->press_subject;
+                    $email->fromName = $press->press_sender_name;
+                    $email->fromEmail = $press->press_sender_email;
+                    $email->replyEmail = $press->press_replyto_email;
+                    
+                    $email->toName = $contact->contact_name_last . ' ' . $contact->contact_name_first;
+                    $email->toEmail = $contact->contact_email;
+                    //print_r( $content);
+                    $result = $email->sendEmail();
+                }
+            }
+        }
 
- public function actionOutbound()
- {
-     $presses = Press::model()->findAll();
-     foreach ($presses as $press){
-         $date=date("Y-m-d H:i:s");
-         if(($press->press_status=='Q')&&($press->press_date_started <= $date)){
-         $list= $press->list;
-         $contacts =$list->contacts;
-         
-         foreach ($contacts as $contact){
-            
-             $email = Yii::app()->mandrillwrap; 
-             //$email->html = $press->press_content;
-//             $email->subject = $press->press_subject;
-//             $email->fromName = $press->press_sender_name;
-//             $email->fromEmail = $press->press_sender_email;
-//             $email->replyEmail=$press->press_replyto_email;
-//             $email->toName = $contact->contact_name_last .' '.$contact->contact_name_first;
-//             $email->toEmail = $contact->contact_email;
-    
-         $result=$email->sendEmail();
-         }
-     }}
-     
-      $this->render('outbound');
-      
-      
- }
-
-
+        $this->render('outbound');
+    }
 
 }
--- protected/extensions/mandrillwrap/mandrillwrap.php
+++ protected/extensions/mandrillwrap/mandrillwrap.php
@@ -1,37 +1,62 @@
 <?php
 
 class mandrillwrap extends CApplicationComponent {
-	
-	public $mandrillKey;
-	public $fromEmail;
-	public $fromName;
-	public $toEmail;
-	public $toName;
-	public $text;
-	public $subject;
-	
-	public function init()
-	{
-		Yii::import('application.vendors.Mandrill');
-		define('MANDRILL_API_KEY','vx4a-QQzpKypkAqF2U9cfA');
-        }
-		public function sendEmail() {		
-		
-$request_json = '{
-"type":"messages",
-"call":"send",
-"message":{"html": "<h1>example html</h1>",
- "text": "example text", 
- "subject": "example subject", 
- "from_email": "mohamedaymen.mastouri@esprit.tn", 
- "from_name": "example from_name",
- "to":[{"email": "mohamedaymen.mastouri@gmail.com", "name": "Wes Widner"}],
- "headers":{"...": "..."},
-"track_opens":false,
-"track_clicks":false
-}}';
-$ret = Mandrill::call((array) json_decode($request_json));
-//$ret1 =Mandrill::call(array('type'=>'users', 'call'=>'ping'));
 
+    public $mandrillKey;
+    public $fromEmail;
+    public $fromName;
+    public $toEmail;
+    public $toName;
+    public $text;
+    public $html;
+    public $subject;
+    public $replyEmail;
+
+    public function init() {
+        Yii::import('application.vendors.Mandrill');
+        define('MANDRILL_API_KEY', 'vx4a-QQzpKypkAqF2U9cfA');
     }
+
+    public function sendEmail() {
+
+        $request_json = '{
+						"type":"messages",
+						"call":"send",
+						"message": {
+                                                        "html": "' . $this->html . '",
+							"subject":"' . $this->subject . '",
+							"to":[
+								{
+								"email": "' . $this->toEmail . '",
+								"name": "' . $this->toName . '"
+								}
+							],
+							"headers":
+								{
+							 "Reply-To": "' . $this->replyEmail . '"
+							},
+							"url_strip_qs":true,
+							"from_email": "' . $this->fromEmail . '",
+							"from_name": "' . $this->fromName . '",
+							"text": "' . $this->text . '",
+							"track_opens":false,
+							"track_clicks":false,
+							"auto_text":true,
+							"tags":["fal"],
+							"google_analytics_domains":["..."],
+							"google_analytics_campaign":["..."],
+							"metadata":["..."],
+                                                         "images": [
+                                                                      {
+                                                           "type": "image/png",
+                                                           "name": "IMAGECID",
+                                                           "content": "ZXhhbXBsZSBmaWxl"
+                                                                             }
+                                                                    ],
+							}
+						}';
+
+        $ret = Mandrill::call((array) json_decode($request_json));
+    }
+
 }
